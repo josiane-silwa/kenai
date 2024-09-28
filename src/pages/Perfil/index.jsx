beb49ae820b6b21d006d578963359e7d2bf0d215
josiane-silwa <108165656+josiane-silwa@users.noreply.github.com>
2024-09-27 21:42:04 -0300

    Alinhamento do carrossel e criação da pagina de perfil

--- src/pages/Perfil/index.jsx
+++ src/pages/Perfil/index.jsx
@@ -1,10 +1,34 @@
 import Brend from '../../components/Brend/index'
 import styles from './Perfil.module.css'
+import fig1 from '../../assets/img/avatar-1.png'
+import fig2 from '../../assets/img/avatar-2.png'
+import fig3 from '../../assets/img/avatar-3.png'
 
 const Perfil = () => {
   return (
     <div className={styles.container}>
       <Brend />
+      <div className={styles.bodyContainer}>
+        <h1>Bem-vindo de volta ao Kenai</h1>
+        <h4>Quem está usando</h4>
+        <div className={styles.avataresContainer}>
+          <div className={styles.avatar}>
+            <img src={fig1} alt="" />
+            <span>Haissa Silva</span>
+          </div>
+          <div className={styles.avatar}>
+            <img src={fig2} alt="" />
+            <span>Matheus Ramos</span>
+          </div>
+          <div className={styles.avatar}>
+            <img src={fig3} alt="" />
+            <span>Viniciius Eiji</span>
+          </div>
+        </div>
+          <div className={styles.button}>
+            <button>Adicionar</button>
+          </div>
+      </div>
     </div>
   )
 }
